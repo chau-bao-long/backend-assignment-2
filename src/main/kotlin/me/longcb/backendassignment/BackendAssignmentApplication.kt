583c8f89c741a262ed81d08a64cd6ed413301a63
chau.bao.long <chau.bao.long@framgia.com>
2019-09-01 10:47:23 +0700

    Initial commit.

---
+++ src/main/kotlin/me/longcb/backendassignment/BackendAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package me.longcb.backendassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendAssignmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/me/longcb/backendassignment/BackendAssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.longcb.backendassignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendAssignmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
